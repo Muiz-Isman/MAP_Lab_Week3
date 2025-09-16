3decc353fb195c937740a457abf1495a89a1385b
Muiz-Isman <muizisman511@gmail.com>
2025-09-16 21:20:44 +0700

    Commit No. 2 – add coffee list & detail fragment

---
+++ app/src/main/java/com/example/map_lab_week3/ListFragment.kt
@@ -0,0 +1,55 @@
+package com.example.map_lab_week3
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
+import android.view.View
+import android.view.ViewGroup
+
+
+class ListFragment : Fragment(), View.OnClickListener{
+    private var param1: String? = null
+    private var param2: String? = null
+    private lateinit var coffeeListener: CoffeeListener;
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if(context is CoffeeListener){
+            coffeeListener = context
+        }
+        else{
+            throw RuntimeException("Must implement CoffeeListener")
+        }
+    }
+   
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val coffeeList = listOf<View>(
+            view.findViewById(R.id.affogato),
+            view.findViewById(R.id.americano),
+            view.findViewById(R.id.latte)
+        )
+        coffeeList.forEach{
+            it.setOnClickListener(this)
+        }
+    }
+    override fun onClick(v: View?) {v?.let{
+            coffee -> coffeeListener.onSelected(coffee.id)
+    }
+
+
+    }
+    companion object {
+
+    }
+}
+
